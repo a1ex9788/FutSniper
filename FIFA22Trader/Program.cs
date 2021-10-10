e6273ecb06f6d4c3b253b88f2b7229eee86a15e1
Alejandro <alejandrocarrion1999@gmail.com>
2021-10-10 18:09:54 +0200

    Entrar a la página principal.

--- FIFA22Trader/Program.cs
+++ FIFA22Trader/Program.cs
@@ -1,12 +1,89 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading.Tasks;
 
-namespace FIFA22Trader
+namespace WhatsAppConnectionMonitorer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public async static Task Main()
         {
-            Console.WriteLine("Hello World!");
+            IWebDriver browser = null;
+
+            try
+            {
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = true;
+
+                browser = new ChromeDriver(service)
+                {
+                    Url = "https://www.ea.com/es-es/fifa/ultimate-team/web-app/",
+                };
+
+                //browser.Manage().Window.Maximize();
+
+                Console.WriteLine("FIFA 22 Trader started.");
+
+                Console.WriteLine("Waiting for sing in...");
+
+                await WaitForSingIn(browser);
+
+                Console.WriteLine("Sing in completed successfully. Main page reached.");
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"An unexpected error occurred: {e.Message}");
+            }
+            finally
+            {
+                browser?.Dispose();
+            }
+        }
+
+        private static async Task WaitForSingIn(IWebDriver browser)
+        {
+            IWebElement singInButton = null;
+
+            do
+            {
+                try
+                {
+                    singInButton = browser.FindElement(By.XPath(".//*[@class='btn-standard call-to-action']"));
+
+                    break;
+                }
+                catch
+                {
+                    Console.Error.WriteLine("Login not completed yet. Please, sing in.");
+                }
+
+                await Task.Delay(1000);
+            }
+            while (singInButton == null);
+
+            singInButton.Click();
+
+            IWebElement startMenuLabel = null;
+
+            do
+            {
+                try
+                {
+                    startMenuLabel = browser.FindElement(By.XPath(".//*[@class='title']"));
+
+                    break;
+                }
+                catch
+                {
+                    Console.Error.WriteLine("Main page not reached yet. Please, sing in.");
+                }
+
+                await Task.Delay(1000);
+            }
+            while (startMenuLabel == null);
         }
     }
 }
