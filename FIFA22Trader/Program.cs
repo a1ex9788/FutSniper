45bc2222997ce5143a4a4354ee8b0a6348cb6155
Alejandro <alejandrocarrion1999@gmail.com>
2021-10-10 18:03:55 +0200

    Add project files.

---
+++ FIFA22Trader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FIFA22Trader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Web scraper for monitoring the connection of a WhatsApp contact.
+
+Selenium (https://www.nuget.org/packages/Selenium.WebDriver) is used for the function of web scraper.
