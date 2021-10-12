5c792e55dada73ae9ec323b69b488783f16510ec
Alejandro <alejandrocarrion1999@gmail.com>
2021-10-12 20:21:41 +0200

    Crear de nuevo MaxPurchasePriceObtainer.

--- FIFA22Trader/WantedPlayersObtainer.cs
+++ FIFA22Trader/WantedPlayersObtainer.cs
@@ -5,10 +5,6 @@ namespace FIFA22Trader
 {
     public static class WantedPlayersObtainer
     {
-        private const int MinPurchasePrice = 200;
-
-        private static readonly Random random = new Random();
-
         public static (string name, int maxPurchasePrice) GetWantedPlayer()
         {
             ConfigurationManager.RefreshSection("appSettings");
@@ -16,32 +12,9 @@ namespace FIFA22Trader
             string wantedPlayer = ConfigurationManager.AppSettings.Get("WantedPlayer");
             int maxPurchasePrice = Convert.ToInt32(ConfigurationManager.AppSettings.Get("MaxPurchasePrice"));
 
-            maxPurchasePrice = CalculateMaxPurchasePrice(maxPurchasePrice);
+            maxPurchasePrice = MaxPurchasePriceObtainer.CalculateMaxPurchasePrice(maxPurchasePrice);
 
             return (wantedPlayer, maxPurchasePrice);
-        }
-
-        private static int CalculateMaxPurchasePrice(int maxPurchasePrice)
-        {
-            int finalMaxPurchasePrice = maxPurchasePrice;
-
-            // Changing the price used in order to avoid results caching.
-            if (GetHalfProbability())
-            {
-                finalMaxPurchasePrice -= 100;
-            }
-
-            if (finalMaxPurchasePrice < MinPurchasePrice)
-            {
-                finalMaxPurchasePrice = MinPurchasePrice;
-            }
-
-            return finalMaxPurchasePrice;
-        }
-
-        private static bool GetHalfProbability()
-        {
-            return random.Next(0, 2) == 0;
         }
     }
 }
