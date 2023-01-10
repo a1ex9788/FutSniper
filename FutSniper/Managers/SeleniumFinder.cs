7a81db52d1b655311b30191a342614522966b731
a1ex9788 <alejandrocarrion1999@gmail.com>
2023-01-10 19:52:56 +0100

    Eliminar catch de los reintentos.

--- FutSniper/Managers/SeleniumFinder.cs
+++ FutSniper/Managers/SeleniumFinder.cs
@@ -17,39 +17,27 @@ namespace FutSniper.Managers
 
         public static async Task<IEnumerable<IWebElement>> FindHtmlElements(ISearchContext searchContext, string xPathSentence, string retryMessage = null, int retries = 0)
         {
-            IEnumerable<IWebElement> htmlElements = null;
-
-            int currentReties = 0;
+            int currentRetry = 0;
 
             do
             {
-                try
-                {
-                    htmlElements = searchContext.FindElements(By.XPath(xPathSentence));
+                IEnumerable<IWebElement> htmlElements = searchContext.FindElements(By.XPath(xPathSentence));
 
-                    if (htmlElements != null && htmlElements.Any())
-                    {
-                        return htmlElements;
-                    }
-                }
-                catch
+                if (htmlElements != null && htmlElements.Any())
                 {
-                    if (retryMessage != null)
-                    {
-                        Console.Error.WriteLine(retryMessage);
-                    }
+                    return htmlElements;
                 }
 
-                if (currentReties++ == retries)
+                if (retryMessage != null)
                 {
-                    throw new Exception($"The retries limit has been reached.");
+                    Console.Error.WriteLine(retryMessage);
                 }
 
                 await Task.Delay(1000);
             }
-            while (htmlElements == null || !htmlElements.Any());
+            while (currentRetry++ < retries);
 
-            return htmlElements;
+            throw new InvalidOperationException($"The retries limit has been reached.");
         }
 
         public static async Task<IWebElement> FindHtmlElementByClass(ISearchContext searchContext, string wantedClass, string retryMessage = null, int retries = 0)
